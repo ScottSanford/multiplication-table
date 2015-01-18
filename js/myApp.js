0425002d55f1102a9a8b839b509a0b84dfa43125
Scott Sanford <scottsanford@Scotts-MacBook-Pro.local>
2015-01-18 16:01:39 -0600

    Add dynamic color and scope events

--- js/myApp.js
+++ js/myApp.js
@@ -1,5 +1,11 @@
 angular.module("myApp", [])
-	.controller("MultiplicationCtrl", function($scope, $attrs) {
+	.controller("DisplayCtrl", function($scope) {
+		$scope.$on('displayData', function(event, data) {
+			$scope.content = data;
+		});
+	})
+
+	.controller("MultiplicationCtrl", function($scope, $attrs, $rootScope) {
 		function populateNumbers(x) {
 			var numbers = [];
 			for (var i=0; i<x; i++) {
@@ -12,11 +18,28 @@ angular.module("myApp", [])
 			return a * b;
 		}
 
-		$scope.$watch('numberLimit', function(limit){
+		$scope.$watch('numberLimit', function(limit) {
 			$scope.numbers = populateNumbers(limit);
 		});
 
 		$scope.numberLimit = $attrs.initialNumberLimit || 10;
-		$scope.numbers = populateNumbers($scope.numberLimit);
+
+		var activeFactorA, activeFactorB;
+		$scope.setActiveFactors = function(a, b) {
+			activeFactorA = a;
+			activeFactorB = b;
+		};
+
+		$scope.matchesFactor = function(a , b) {
+			return a === activeFactorA || b === activeFactorB;
+		};
+
+		$scope.clearActiveFactors = function() {
+    		activeFactorA = activeFactorB = null;
+		};
+
+		$scope.setActiveNumber = function(number) {
+			$rootScope.$broadcast('displayData', number);
+		};
 
 	});
